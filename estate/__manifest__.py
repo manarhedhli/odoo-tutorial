cdf21ff216bc8c9fc0d92e8440cb6e49e2cc6501
Manar <hedhlii.manar@gmail.com>
2025-02-22 22:36:10 +0100

    Implementation of the Define module data tutorial

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,10 +5,8 @@
         'account',
         'contacts'
     ],
-
     'data': [
         'security/ir.model.access.csv',
-
         'views/inherited_users_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
@@ -16,7 +14,11 @@
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
     ],
-
+    'demo': [
+        'demo/estate.property.type.csv',
+        'demo/estate_property.xml',
+        'demo/estate_property_offer.xml',
+    ],
     'installable': True,
     'application': True,
     'auto_install': False
